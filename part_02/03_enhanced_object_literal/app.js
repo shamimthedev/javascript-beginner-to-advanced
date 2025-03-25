7dc6f79092d76a31db93a0c87218fa710b360b39
Md. Shamim Hossain <smdshamimhossain675@gmail.com>
2025-03-26 04:09:47 +0600

    Updated

--- part_02/03_enhanced_object_literal/app.js
+++ part_02/03_enhanced_object_literal/app.js
@@ -1,11 +1,27 @@
-let a = 2
-let b = 3
-let c = 4
+let a = 2;
+let b = 3;
+let c = 4;
 
 let obj = {
-    a,
-    b,
-    c
+  a,
+  b,
+  c,
+};
+
+// console.log(obj)
+
+const lib = {
+  sum: (a, b) => a + b,
+  mult: (a, b) => a * b,
+};
+
+// console.log(lib.sum(2, 3));
+// console.log(lib.mult(2, 3));
+
+function getPersonES6(name, age, height){
+    return {
+        name, age, height 
+    }
 }
 
-console.log(obj)
+console.log(getPersonES6('Shamim', 28, 5.6))
