95dc296fed963a3f773c0964fdb884469a587175
Md. Shamim Hossain <smdshamimhossain675@gmail.com>
2025-03-30 19:37:41 +0600

    Added Project-11

---
+++ projects/11_signup_form/script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector('.container')
+const signIn = document.querySelector('.signin')
+const signUp = document.querySelector('.signup')
+const submit = document.querySelector('.submit')
+
+signIn.addEventListener('click',()=>{
+    container.classList.add('change')
+    submit.textContent = 'Sign In'
+})
+signUp.addEventListener('click',()=>{
+    container.classList.remove('change')
+    submit.textContent = 'Sign Up'
+})
