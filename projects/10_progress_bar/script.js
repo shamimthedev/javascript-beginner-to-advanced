2a736f999518ce3047d1ae827aa21b60def26552
Md. Shamim Hossain <smdshamimhossain675@gmail.com>
2025-03-30 16:38:44 +0600

    Added Project-10

---
+++ projects/10_progress_bar/script.js
@@ -0,0 +1,10 @@
+const downloadBtn = document.querySelector('.download')
+const resetBtn = document.querySelector('.reset')
+const container = document.querySelector('.container')
+
+downloadBtn.addEventListener('click',()=>{
+    container.classList.add('change')
+})
+resetBtn.addEventListener('click',()=>{
+    container.classList.remove('change')
+})
